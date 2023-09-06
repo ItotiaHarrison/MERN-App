53b8d9b399f51d164afd3294424c102c04ea72cb
Harrison Itotia <harrisonitotia@gmail.com>
2023-09-06 09:50:58 +0300

    complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About</h1>
+
+      <p className='mb-4 text-slate-700'>
+
+      </p>
+
+      <p className='mb-4 text-slate-700'>
+
+      </p>
+      
+      <p className='mb-4 text-slate-700'>
+
+      </p>
+    </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to my Auth App!</h1>
+
+      <p className='mb-4 text-slate-700'>
+
+      </p>
+
+      <p className='mb-4 text-slate-700'>
+
+      </p>
+      
+      <p className='mb-4 text-slate-700'>
+
+      </p>
+    </div>
   )
 }
